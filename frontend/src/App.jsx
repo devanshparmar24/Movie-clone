020c0fddbffc009d0dcff628e6ff5a0a54c17156
devanshparmar24 <devanshparmar13618@gmail.com>
2025-12-01 14:44:34 +0530

    initial commit and search is working using state variables

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieCard from './components/MovieCard.jsx';
+import Home from './pages/home.jsx';
+
+function App() {
+  return (
+    <>
+      <Home />        
+    </>
+  )
+}
+
+export default App
