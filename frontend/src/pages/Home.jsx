fc33b6a958d6373d0e1d89b0fc2f588392f497bc
devanshparmar24 <devanshparmar13618@gmail.com>
2025-12-01 19:06:59 +0530

    Added fun images (inside joke)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,19 @@ import MovieCard from '../components/MovieCard.jsx';
 import { useState } from 'react';
 import '../css/Home.css'
 
+import imgFarewell from '../assets/Adoption-Centre-Farewell.jpeg';
+import imgHarit from '../assets/Harit.jpeg';
+import imgOgs from '../assets/Adoption-Centre-TheOgs.jpeg';
+import imgChads from '../assets/Adoption-Centre-TheChadsunite.jpeg';
+
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
 
     const movies = [
-        { id: 1, title: "Genius", release_date: "2022", },
-        { id: 2, title: "Gadar", release_date: "2023", },
-        { id: 3, title: "Garam Masala", release_date: "2005", },
-        { id: 4, title: "Fukrey", release_date: "2014", }
+        { id: 1, title: "Adoption Center- Farewell", release_date: "2022", url: imgFarewell },
+        { id: 2, title: "Harit if he keeps talking", release_date: "2025", url: imgHarit },
+        { id: 3, title: "Adoption Centre- The Ogs", release_date: "2019", url: imgOgs },
+        { id: 4, title: "Adoption Centre- The Chads Unite", release_date: "2024", url: imgChads }
     ];
 
 
